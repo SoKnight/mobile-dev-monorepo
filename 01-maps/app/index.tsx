2b1cf560b42428f01bdd6573aae7e333816c9936
SoKnight <mcliyanage2018@gmail.com>
2025-10-24 23:01:48 +0500

    feat: add initial map display

--- 01-maps/app/index.tsx
+++ 01-maps/app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { useRouter } from "expo-router";
+
+import Map from "@/components/Map"
+import { globalMapMarkerList, globalMapMarkerStore, MapMarkerList, nextMapMarkerId } from "@/storage/InMemoryStorage";
+import { MapMarkerModel } from "@/types";
 
 export default function Index() {
+  const [markers, setMarkers] = useState<MapMarkerList>(globalMapMarkerList)
+  const router = useRouter()
+  
+  const onMapViewLongPress = (event: any) => {
+    const marker: MapMarkerModel = {
+      id: nextMapMarkerId(),
+      location: event.coordinate,
+    }
+    
+    globalMapMarkerStore.set(marker.id, marker)
+    setMarkers([...markers, marker])
+  }
+  
+  const onMapMarkerPress = (id: number) => {
+    // TODO code
+  }
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+    <Map markers={markers} onMapViewLongPress={onMapViewLongPress} onMapMarkerPress={onMapMarkerPress}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
