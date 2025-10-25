944a507aa1bb187e00e02aa9be8b34466b22d7cb
SoKnight <mcliyanage2018@gmail.com>
2025-10-25 10:05:12 +0500

    chore: do not wrap single Stack instance into array

--- 01-maps/app/_layout.tsx
+++ 01-maps/app/_layout.tsx
@@ -2,11 +2,9 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown: false}} />
-        <Stack.Screen name="marker/[id]" options={{headerTitle: 'Маркер', statusBarStyle: 'dark'}}/>
-      </Stack>
-    </>
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}} />
+      <Stack.Screen name="marker/[id]" options={{headerTitle: 'Маркер', statusBarStyle: 'dark'}}/>
+    </Stack>
   );
 }
