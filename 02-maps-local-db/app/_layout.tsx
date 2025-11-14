b316a8d136d0caa4ee5bdc7b48f6fbd023112cb4
SoKnight <mcliyanage2018@gmail.com>
2025-11-15 01:30:55 +0500

    feat: provide DatabaseContext

--- 02-maps-local-db/app/_layout.tsx
+++ 02-maps-local-db/app/_layout.tsx
@@ -1,10 +1,13 @@
+import { DatabaseProvider } from "@/context/DatabaseContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}} />
-      <Stack.Screen name="marker/[id]" options={{headerTitle: 'Маркер', statusBarStyle: 'dark'}}/>
-    </Stack>
+    <DatabaseProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{headerShown: false}} />
+        <Stack.Screen name="marker/[id]" options={{headerTitle: 'Маркер', statusBarStyle: 'dark'}}/>
+      </Stack>
+    </DatabaseProvider>
   );
 }
