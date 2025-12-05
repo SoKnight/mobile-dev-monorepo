9fc70c06569c35a6fa76b1d0fdbe9e811a4fead5
SoKnight <mcliyanage2018@gmail.com>
2025-12-06 00:50:50 +0500

    feat: implement LocationService#lookupMarkersNearby

--- 03-maps-pushes/services/location.ts
+++ 03-maps-pushes/services/location.ts
@@ -1,6 +1,11 @@
 import * as Location from "expo-location";
 import {LatLng} from "react-native-maps";
-import {PermissionAwareService} from "./services";
+import {MapMarkerList} from "@/types";
+import {PermissionAwareService} from "@/services/services";
+import {notificationService} from "@/services/notifications";
+
+let EARTH_RADIUS = 6371e3;
+let NEARBY_THRESHOLD = 100;
 
 let DEFAULT_CONFIG: LocationConfig = {
     accuracy: Location.Accuracy.Balanced,
@@ -25,6 +30,17 @@ class LocationService implements PermissionAwareService {
         this.config = { ...DEFAULT_CONFIG, ...config };
         this.location = null;
         this.subscription = null;
+    }
+
+    public async lookupMarkersNearby(currentlyAt: LatLng, markers: MapMarkerList): Promise<void> {
+        await Promise.all(markers.map(marker => {
+            const distance = this.calculateDistance(currentlyAt, marker.location);
+            if (distance <= NEARBY_THRESHOLD) {
+                return notificationService.showNotification(marker);
+            } else {
+                return notificationService.removeNotification(marker.id);
+            }
+        }));
     }
 
     public async obtainCurrentLocation(): Promise<LatLng | null> {
@@ -68,6 +84,22 @@ class LocationService implements PermissionAwareService {
         if (background !== 'granted') {
             throw new Error('Location (background) permission denied!');
         }
+    }
+
+    private calculateDistance(from: LatLng, to: LatLng): number {
+        const toRadians = (deg: number) => (deg * Math.PI) / 180;
+
+        const phi1 = toRadians(from.latitude);
+        const phi2 = toRadians(to.latitude);
+        const phiD = toRadians(to.latitude - from.latitude);
+        const lambdaD = toRadians(to.longitude - from.longitude);
+
+        const sinHalfPhiD = Math.sin(phiD / 2);
+        const sinHalfLambdaD = Math.sin(lambdaD / 2);
+
+        const a = sinHalfPhiD * sinHalfPhiD + Math.cos(phi1) * Math.cos(phi2) * sinHalfLambdaD * sinHalfLambdaD;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return EARTH_RADIUS * c;
     }
 
 }
